a83d61e2be3f98b9f4da4b81bfcf0069bfbb9111
nishtha <nishthathakkar0502@gmail.com>
2020-11-30 21:45:15 +0530

    Weather Dashboard

---
+++ Dashboard/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from Dashboard.forms import CityForm
+
+import requests
+# Create your views here.
+def home(request):
+	form = CityForm()
+
+	if request.method == 'POST':
+		#DO something
+		form = CityForm(request.POST)
+		if form.is_valid():
+			form.save()
+			city_name = form.cleaned_data.get('city_name')
+
+			url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid=6ff9d59744b255f51196e7fea911f1d3'
+			response = requests.get(url)
+			json_response =  response.json()
+
+            weather_data = {
+		      'temp': json_response['main']['temp'],
+		      'temp_min': json_response['main']['temp_min'],
+		      'temp_max':json_response['main']['temp_max'],
+		      'city_name': json_response['name'],
+		      'country': json_response['sys']['country'],
+		      'lat': json_response['coord']['lat'],
+		      'lon': json_response['coord']['lon'],
+		      'weather': json_response['weather'][0]['main'],
+		      'weather_desc': json_response['weather'][0]['description'],
+		      'pressure': json_response['main']['pressure'],
+		      'humidity': json_response['main']['humidity'],
+		      'wind_speed': json_response['wind']['speed'],
+	        } 
+	  elif request.method == 'GET':
+		weather_data = None
+	template_name = 'home.html'
+	context = {'form': form, 'weather_data': weather_data}
+	return render(request,template_name,context=context)
