e3cabc949d4cbf66265b1d6c84902d33492b7a42
Gaurav Sharma <34805677+hamhaingaurav@users.noreply.github.com>
2020-11-30 21:55:14 +0530

    Corrected the indentation: Gaurav

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -18,20 +18,20 @@ def home(request):
 			response = requests.get(url)
 			json_response =  response.json()
 
-            weather_data = {
-		      'temp': json_response['main']['temp'],
-		      'temp_min': json_response['main']['temp_min'],
-		      'temp_max':json_response['main']['temp_max'],
-		      'city_name': json_response['name'],
-		      'country': json_response['sys']['country'],
-		      'lat': json_response['coord']['lat'],
-		      'lon': json_response['coord']['lon'],
-		      'weather': json_response['weather'][0]['main'],
-		      'weather_desc': json_response['weather'][0]['description'],
-		      'pressure': json_response['main']['pressure'],
-		      'humidity': json_response['main']['humidity'],
-		      'wind_speed': json_response['wind']['speed'],
-	        } 
+			weather_data = {
+				'temp': json_response['main']['temp'],
+				'temp_min': json_response['main']['temp_min'],
+				'temp_max':json_response['main']['temp_max'],
+				'city_name': json_response['name'],
+				'country': json_response['sys']['country'],
+				'lat': json_response['coord']['lat'],
+				'lon': json_response['coord']['lon'],
+				'weather': json_response['weather'][0]['main'],
+				'weather_desc': json_response['weather'][0]['description'],
+				'pressure': json_response['main']['pressure'],
+				'humidity': json_response['main']['humidity'],
+				'wind_speed': json_response['wind']['speed'],
+			} 
 	  elif request.method == 'GET':
 		weather_data = None
 	template_name = 'home.html'
